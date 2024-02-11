f4645a3a60df2b74389b1702891f38b4c70fe371
Shaurya Agarwal <48658455+ShauryaAgarwal@users.noreply.github.com>
2024-02-11 08:56:11 -0500

    Update generate_audiogram.py

--- api/generate_audiogram.py
+++ api/generate_audiogram.py
@@ -11,7 +11,7 @@ class handler(BaseHTTPRequestHandler):
         # Process data here to generate audiogram
 
         # Placeholder image URL
-        image_url = "https://placekitten.com/200/300"
+        image_url = "https://cdn.discordapp.com/attachments/1205691470951088199/1206234494667853844/image.png?ex=65db446a&is=65c8cf6a&hm=3c37da74d1ba751b4ac1e2918703bce7bbc6f069f597543caea600cae662fa4e&"
 
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
